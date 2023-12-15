b12bc70c5cbdc3802e8e8366caa9e564e4770ac5
mritunjayRoy <mroy8202@gmail.com>
2023-12-15 21:39:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv").config();
+const fileupload = require("express-fileupload");
+const db = require("./config/database");
+const cloudinary = require("./config/cloudinary");
+const Upload = require("./routes/FileUpload");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+// middleware to upload file at server
+app.use(fileupload());
+
+// dbconnect
+db.connect();
+
+// cloudinary
+cloudinary.cloudinaryConnect();
+
+// mount api routes
+app.use("/api/v1/upload", Upload);
+
+// activate server
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+});
